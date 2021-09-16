2316d0aa02218aef86f110298f1750f299973e52
JoYongHo <adauy@daum.net>
2021-09-16 14:44:31 +0900

    스터디방 만들기 백단구현

--- src/study/controller/createStudyRoom.java
+++ src/study/controller/createStudyRoom.java
@@ -1,4 +1,4 @@
-package study;
+package study.controller;
 
 import java.io.IOException;
 
@@ -9,17 +9,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import study.model.vo.Study;
+
 /**
- * Servlet implementation class EnterStudyRoom
+ * Servlet implementation class createStudyRoom
  */
-@WebServlet("/study/enterStudy")
-public class EnterStudyRoom extends HttpServlet {
+@WebServlet("/study/createStudy")
+public class createStudyRoom extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public EnterStudyRoom() {
+    public createStudyRoom() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -28,7 +30,8 @@ public class EnterStudyRoom extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher view= request.getRequestDispatcher("/WEB-INF/views/study/innerStudyRoom.jsp");
+		RequestDispatcher view= request.getRequestDispatcher("/WEB-INF/views/study/CreateStudy.jsp");
+		request.setAttribute("nav1", "study");
 		view.forward(request, response);
 	}
 
@@ -36,8 +39,24 @@ public class EnterStudyRoom extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		// 파일 첨부 추가 예정
+		String studyName = request.getParameter("studyName");
+		String studyTO = request.getParameter("studyTO");
+		String[] studyDayArr = request.getParameterValues("studyDay");
+		String studyDay="";
+		if(!studyDayArr[0].equals(""))
+			studyDay = String.join(",", studyDayArr);
+		String cid = request.getParameter("cid");
+		String studyExplain = request.getParameter("studyExplain");
+		
+		
+		Study study = new Study();
 	}
 
 }
+
+
+
+
+
+
